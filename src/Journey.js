4dca22ff3bcc6f019b1bb193187b2fc3a7367c36
Sandrita <sandramilenagarciaa@gmail.com>
2022-02-11 16:11:45 -0800

    fix bugs

--- src/Journey.js
+++ src/Journey.js
@@ -28,75 +28,76 @@ const Journey = () => {
   return (
     <div className="Journey" id="Journey">
       <p>Journey</p>
-      <div>
-        <VerticalTimeline>
-          {experienceData.map((Data) => {
-            return (
-              <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{
-                  // background: "rgb(255, 255, 255)",
-                  background: "rgb(250, 250, 250)",
-                  color: "#fff",
-                }}
-                contentArrowStyle={{
-                  borderRight: "7px solid  rgb(97, 97, 97)",
-                }}
-                lineColor={"rgb(33, 150, 243)"}
-                date={Data.Date}
-                iconStyle={{
-                  background: "rgb(226, 217, 209)",
-                  color: "#fff",
-                }}
-                icon={Data.Icon}
-              >
-                {/* <h3 className="vertical-timeline-element-title">
+
+      <VerticalTimeline>
+        {experienceData.map((Data) => {
+          return (
+            // <div class="work">
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                // background: "rgb(255, 255, 255)",
+                background: "rgb(250, 250, 250)",
+                color: "#fff",
+              }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(97, 97, 97)",
+              }}
+              lineColor={"black"}
+              date={Data.Date}
+              iconStyle={{
+                background: "rgb(226, 217, 209)",
+                color: "#fff",
+              }}
+              icon={Data.Icon}
+            >
+              {/* <h3 className="vertical-timeline-element-title">
                   {Data.Title}
                 </h3> */}
 
-                {/* <h4 className="vertical-timeline-element-subtitle">
+              {/* <h4 className="vertical-timeline-element-subtitle">
                   {Data.Location}
                 </h4> */}
-                {/* 
+              {/* 
                 <Paper
                   sx={{ p: 4, margin: "auto", maxWidth: 500, flexGrow: 1 }}
                 > */}
-                <Grid
-                  container
-                  spacing={1}
-                  sx={{
-                    p: 1,
-                    margin: "auto",
-                    maxWidth: 500,
-                    flexGrow: 1,
-                  }}
-                >
-                  {/* <Grid item xs={12} sm={12} md={12} container>
+              <Grid
+                container
+                spacing={1}
+                sx={{
+                  p: 1,
+                  margin: "auto",
+                  maxWidth: 500,
+                  flexGrow: 1,
+                }}
+              >
+                {/* <Grid item xs={12} sm={12} md={12} container>
                     <Grid item xs container direction="column" spacing={2}> */}
-                  <Grid item>
-                    <h3 className="vertical-timeline-element-title">
-                      {Data.Title}
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                      {Data.Location}
-                    </h4>
+                <Grid item>
+                  <h3 className="vertical-timeline-element-title">
+                    {Data.Title}
+                  </h3>
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {Data.Location}
+                  </h4>
 
-                    <Typography variant="body2">
-                      <p>{Data.Description}</p>
-                      <p>{Data.Description1}</p>
-                      <p>{Data.Description2}</p>
-                      <p>{Data.Description3}</p>
-                    </Typography>
-                    {/* <Img sx={{ width: 100 }} alt="image1" src={Data.Image} /> */}
-                    {/* <Img sx={{ width: 100 }} alt="complex" src={myLogo} />
+                  <Typography variant="body2">
+                    <p>{Data.Description}</p>
+                    <p>{Data.Description1}</p>
+                    <p>{Data.Description2}</p>
+                    <p>{Data.Description3}</p>
+                  </Typography>
+                  {/* <Img sx={{ width: 100 }} alt="image1" src={Data.Image} /> */}
+                  {/* <Img sx={{ width: 100 }} alt="complex" src={myLogo} />
                     <Img sx={{ width: 100 }} alt="complex" src={myLogo} /> */}
-                  </Grid>
                 </Grid>
-                {/* </Grid>
+              </Grid>
+              {/* </Grid>
                 </Grid> */}
-                {/* </Paper> */}
+              {/* </Paper> */}
 
-                {/* <Grid
+              {/* <Grid
                   container
                   rowSpacing={1}
                   columnSpacing={{
@@ -119,49 +120,69 @@ const Journey = () => {
                     4{" "}
                   </Grid>
                 </Grid> */}
-                {/* <p>{Data.Description}</p>
+              {/* <p>{Data.Description}</p>
                 <p>{Data.Description1}</p>
                 <p>{Data.Description2}</p>
                 <p>{Data.Description3}</p> */}
-              </VerticalTimelineElement>
-            );
-          })}
-          {educationData.map((edData) => {
-            return (
-              <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{
-                  background: "rgb(250, 250, 250)",
-                  color: "#fff",
+            </VerticalTimelineElement>
+            // </div>
+          );
+        })}
+
+        {educationData.map((edData) => {
+          return (
+            // <div class="education">
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "rgb(250, 250, 250)",
+                color: "#fff",
+              }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(97, 97, 97)",
+              }}
+              lineColor={"#000"}
+              date={edData.Date}
+              iconStyle={{
+                background: "rgb(238, 224, 191)",
+                color: "#fff",
+              }}
+              icon={edData.Icon}
+            >
+              <Grid
+                container
+                spacing={1}
+                sx={{
+                  p: 1,
+                  margin: "auto",
+                  maxWidth: 500,
+                  flexGrow: 1,
                 }}
-                contentArrowStyle={{
-                  borderRight: "7px solid  rgb(97, 97, 97)",
-                }}
-                lineColor={"rgb(33, 150, 243)"}
-                date={edData.Date}
-                iconStyle={{ background: "rgb(238, 224, 191)", color: "#fff" }}
-                icon={edData.Icon}
               >
-                <h2 className="vertical-timeline-element-title">
-                  {edData.Degree}
-                </h2>
-                <h4 className="vertical-timeline-element-subtitle">
-                  {edData.Major}
-                </h4>
-                <h4 className="vertical-timeline-element-subtitle">
-                  {edData.Honors}
-                </h4>
-                <h2 className="vertical-timeline-element-title">
-                  {edData.School}
-                </h2>
-                <h4 className="vertical-timeline-element-subtitle">
-                  {edData.Location}
-                </h4>
-              </VerticalTimelineElement>
-            );
-          })}
-        </VerticalTimeline>
-      </div>
+                <Grid item>
+                  <h2 className="vertical-timeline-element-title">
+                    {edData.Degree}
+                  </h2>
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {edData.Major}
+                  </h4>
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {edData.Honors}
+                  </h4>
+                  {/* <h2 className="vertical-timeline-element-title">
+                      {edData.School}
+                    </h2> */}
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {edData.Location}
+                  </h4>
+                </Grid>
+              </Grid>
+            </VerticalTimelineElement>
+            // </div>
+          );
+        })}
+      </VerticalTimeline>
+      <div></div>
     </div>
   );
 };
