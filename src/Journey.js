d03077610b37c918070c3fac78be3e5f34f91740
Sandrita <sandramilenagarciaa@gmail.com>
2022-02-21 17:02:35 -0800

    fix journey css

--- src/Journey.js
+++ src/Journey.js
@@ -9,21 +9,8 @@ import { educationData } from "./data";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 // import Paper from "@mui/material/Paper";
-// import { styled } from "@mui/material/styles";
-
-// const Img = styled("img")({
-//   margin: "auto",
-//   display: "block",
-//   maxWidth: "100%",
-//   maxHeight: "100%",
-// });
 
 const Journey = () => {
-  // let dataInfo = { educationData };
-  // console.log(dataInfo);
-  // let dataInfo1 = { experienceData };
-  // console.log(dataInfo1);
-
   return (
     <div className="Journey" id="Journey">
       <p>Journey</p>
@@ -31,11 +18,9 @@ const Journey = () => {
       <VerticalTimeline>
         {experienceData.map((Data) => {
           return (
-            // <div class="work">
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
               contentStyle={{
-                // background: "rgb(255, 255, 255)",
                 background: "rgb(250, 250, 250)",
                 color: "#fff",
               }}
@@ -50,17 +35,6 @@ const Journey = () => {
               }}
               icon={Data.Icon}
             >
-              {/* <h3 className="vertical-timeline-element-title">
-                  {Data.Title}
-                </h3> */}
-
-              {/* <h4 className="vertical-timeline-element-subtitle">
-                  {Data.Location}
-                </h4> */}
-              {/* 
-                <Paper
-                  sx={{ p: 4, margin: "auto", maxWidth: 500, flexGrow: 1 }}
-                > */}
               <Grid
                 container
                 spacing={1}
@@ -71,12 +45,13 @@ const Journey = () => {
                   flexGrow: 1,
                 }}
               >
-                {/* <Grid item xs={12} sm={12} md={12} container>
-                    <Grid item xs container direction="column" spacing={2}> */}
                 <Grid item>
-                  <h3 className="vertical-timeline-element-title">
+                  <h2 className="vertical-timeline-element-title">
                     {Data.Title}
-                  </h3>
+                  </h2>
+                  <h4 className="vertical-timeline-element-title">
+                    {Data.Company}
+                  </h4>
                   <h4 className="vertical-timeline-element-subtitle">
                     {Data.Location}
                   </h4>
@@ -87,42 +62,8 @@ const Journey = () => {
                     <p>{Data.Description2}</p>
                     <p>{Data.Description3}</p>
                   </Typography>
-                  {/* <Img sx={{ width: 100 }} alt="image1" src={Data.Image} /> */}
-                  {/* <Img sx={{ width: 100 }} alt="complex" src={myLogo} />
-                    <Img sx={{ width: 100 }} alt="complex" src={myLogo} /> */}
                 </Grid>
               </Grid>
-              {/* </Grid>
-                </Grid> */}
-              {/* </Paper> */}
-
-              {/* <Grid
-                  container
-                  rowSpacing={1}
-                  columnSpacing={{
-                    p: 2,
-                    margin: "auto",
-                    maxWidth: 500,
-                    flexGrow: 1,
-                  }}
-                >
-                  <Grid item xs={12} md={6}>
-                    1{" "}
-                  </Grid>
-                  <Grid item xs={6}>
-                    2{" "}
-                  </Grid>
-                  <Grid item xs={6}>
-                    3{" "}
-                  </Grid>
-                  <Grid item xs={6}>
-                    4{" "}
-                  </Grid>
-                </Grid> */}
-              {/* <p>{Data.Description}</p>
-                <p>{Data.Description1}</p>
-                <p>{Data.Description2}</p>
-                <p>{Data.Description3}</p> */}
             </VerticalTimelineElement>
             // </div>
           );
@@ -130,7 +71,6 @@ const Journey = () => {
 
         {educationData.map((edData) => {
           return (
-            // <div class="education">
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
               contentStyle={{
@@ -159,27 +99,24 @@ const Journey = () => {
                 }}
               >
                 <Grid item>
-                  <h3 className="vertical-timeline-element-title">
+                  <h2 className="vertical-timeline-element-title">
                     {edData.Degree}
-                  </h3>
+                  </h2>
                   <h4 className="vertical-timeline-element-subtitle">
-                    {edData.Major}
+                    {edData.Major} {edData.Honors}
                   </h4>
-                  <h4 className="vertical-timeline-element-subtitle">
+                  {/* <h4 className="vertical-timeline-element-subtitle">
                     {edData.Honors}
+                  </h4> */}
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {edData.School}
                   </h4>
-                  {/* <h2 className="vertical-timeline-element-title">
-                      {edData.School}
-                    </h2> */}
                   <h4 className="vertical-timeline-element-subtitle">
                     {edData.Location}
                   </h4>
-
-                  {/* <Img sx={{ width: 100 }} alt="image1" src={edData.Image} /> */}
                 </Grid>
               </Grid>
             </VerticalTimelineElement>
-            // </div>
           );
         })}
       </VerticalTimeline>
